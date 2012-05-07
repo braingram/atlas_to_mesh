ef129bc538fc543a1dab61fc520188c31e11b763
Brett Graham <graham@rowland.harvard.edu>
2012-05-07 13:14:01 -0400

    removing old cruft, adding new construct_areas python script

--- construct_areas.py
+++ construct_areas.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-import pickle
+import cPickle as pickle
 import sys
 
 import pylab
 
 import section
-#import points
 
 
 def construct_areas(sindexes, areas, epsdir='eps/', tmpdir='tmp/', \
@@ -17,10 +16,9 @@ def construct_areas(sindexes, areas, epsdir='eps/', tmpdir='tmp/', \
     for area in areas:
         pts[area] = []
         for s in sections:
-            pts[area] += s.find_area(area, 'skull')
-
-    with open('areas.p', 'w') as ofile:
-        pickle.dump(pts, ofile)
+            pts[area] += [[p.x, p.y, s.get_ap()] for p in \
+                    s.find_area(area, 'skull')]
+    return pts
 
 
 def show_sections(sindexes, areas, epsdir='eps/', tmpdir='tmp/'):
@@ -33,9 +31,14 @@ def show_sections(sindexes, areas, epsdir='eps/', tmpdir='tmp/'):
 
 if __name__ == '__main__':
     areas = ['V2L', 'AuD', 'Au1', 'AuV', 'PRh', 'V1B', 'V1M', 'TeA', 'Ect']
+    #areas = ['TeA']
     # skip 22, 47, 76, 144, 148
     sis = [i for i in range(12, 162) if not (i in [22, 47, 76, 144, 148])]
     if len(sys.argv) > 1:
         sis = [int(sys.argv[1])]
+    if len(sys.argv) > 2:
+        areas = [sys.argv[2]]
     #show_sections(sis, areas=areas, epsdir='/home/graham/Desktop/eps/')
-    construct_areas(sis, areas=areas, epsdir='/home/graham/Desktop/eps/')
+    pts = construct_areas(sis, areas=areas, epsdir='atlas/eps/')
+    with open('areas.p', 'w') as ofile:
+        pickle.dump(pts, ofile)
